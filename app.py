a4304233289ddebde451805953f181abd24fb299
John Rothera <johnwrothera@gmail.com>
2025-03-05 17:20:32 +0000

    fixed app.py to pass homepage test.

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def get_home():
     return "Hello, world!"
 
-if __name__ == '__main__':
-    app.run(
-      debug=True,
-      host="0.0.0.0" # Listen for connections _to_ any server
-    )
+
+@app.route("/home", methods=["GET"])
+def get_homepage():
+    return "Homepage"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")  # Listen for connections _to_ any server
