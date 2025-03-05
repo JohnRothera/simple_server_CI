8b0acd123872357eb2125b5b59aea5929b8f5a19
John Rothera <johnwrothera@gmail.com>
2025-03-05 17:16:15 +0000

    added home page test, test fails.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,9 @@ def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Hello, world!"
+
+
+def test_new_feature_fails(web_client):
+    response = web_client.get("/home")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "Homepage"
